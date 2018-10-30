356a8d2d1338a57113baaac05b95f88a357cdc31
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-10-30 14:01:14 +0000

    Implement image loader

--- ImageGallery/gallery/ImageCell.swift
+++ ImageGallery/gallery/ImageCell.swift
@@ -4,36 +4,58 @@ import UIKit
 class ImageCell: UICollectionViewCell {
     @IBOutlet weak var progressIndicator: UIActivityIndicatorView!//TODO remove
     @IBOutlet weak var image: UIImageView!
-    private var url: URL? = nil
+    private var task: Task?
 
     func loadImage(_ url: URL) {
-        if (url != self.url) {
-            setNil()
+        if (url != task?.url) {
+            task?.cancel()
+            task = nil
         }
-        self.url = url
-        let closure: (Data?, URLResponse?, Error?) -> () = { (data, response, error) in
-            if (error != nil) {
-                self.setNil()
-                return
-            }
-            if (response?.url != self.url) {
-                self.setNil()
-                return
-            }
-            DispatchQueue.main.async {
-                if let data = data {
-                    if let downloadedImage = UIImage(data: data) {
-                        self.image.image = downloadedImage
+        task = Task(image, url)
+    }
+
+    class Task {
+        var image: UIImageView?
+        var url: URL? = nil
+        var urlSessionDataTask: URLSessionDataTask? = nil
+
+        init(_ image: UIImageView, _ url: URL) {
+            self.image = image
+            self.url = url
+            setNil()
+            let closure: (Data?, URLResponse?, Error?) -> () = { (data, response, error) in
+                if (error != nil) {
+                    self.setNil()
+                    return
+                }
+                if (response?.url != self.url) {
+                    self.setNil()
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            self.image?.image = downloadedImage
+                        }
                     }
                 }
             }
+            urlSessionDataTask = URLSession.shared.dataTask(with: url, completionHandler: closure)
+            urlSessionDataTask?.resume()
         }
-        URLSession.shared.dataTask(with: url, completionHandler: closure).resume()
+
+        private func setNil() {
+            DispatchQueue.main.async {
+                self.image?.image = nil
+            }
+        }
+
+        func cancel() {
+            setNil()
+            urlSessionDataTask?.cancel()
+            self.image = nil
+        }
     }
 
-    private func setNil() {
-        DispatchQueue.main.async {
-            self.image.image = nil
-        }
-    }
+
 }
